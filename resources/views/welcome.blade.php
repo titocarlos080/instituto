1f00df28ff9fac2efaff1730447aa448fb01a7ff
Tito Carlos <titocarlos080@gmail.com>
2024-06-17 10:10:56 -0400

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Instituto</title>
+ </head>
+ <body>
+ 
+ soy el instituto
+ 
+
+ </body>
+ </html>
